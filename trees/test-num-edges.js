b3c24c8f3b449bf43497e6979a6c28ec4cffa9d9
Aldo Caruso <aldocaruso1991@gmail.com>
2020-04-26 17:51:25 +0200

    Update

---
+++ trees/test-num-edges.js
@@ -0,0 +1,15 @@
+const BinarySearchTree = require('./BinarySearchTree');
+
+let tree = new BinarySearchTree();
+
+tree.insert(7);
+tree.insert(5);
+tree.insert(2);
+tree.insert(8);
+tree.insert(9);
+tree.insert(3);
+tree.insert(4);
+tree.insert(10);
+tree.insert(11);
+
+console.log(tree.getNumEdges())
